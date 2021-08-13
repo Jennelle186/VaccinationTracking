6ce643cd9508228b3b188ed8970e3e8c419a5c04
Ceed Jennelle Lorenzo <bg201801625@wmsu.edu.ph>
2021-08-13 14:40:05 +0800

    fixed the dashboard UI

--- src/Admin/Components/Dashboard/dashboard.js
+++ src/Admin/Components/Dashboard/dashboard.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { Grid, makeStyles, Card, Typography } from "@material-ui/core";
+import {
+  Grid,
+  makeStyles,
+  Card,
+  Typography,
+  CardActionArea,
+  CardContent,
+  Paper,
+  CardHeader,
+} from "@material-ui/core";
 import ButtonForm from "../../../components/Forms/Button/button";
 
 const useStyles = makeStyles((theme) => ({
@@ -21,17 +30,36 @@ const Dashboard = () => {
         justifyContent="flex-start"
         alignItems="flex-start"
       >
-        <Grid item xs>
-          <ButtonForm>Scan QR Code</ButtonForm>
+        <Grid item>
+          <Card elevation={10}>
+            <CardContent>
+              <CardContent>
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="h2"
+                ></Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  <Grid container spacing={2}>
+                    <Grid item>
+                      <ButtonForm>Scan QR Code</ButtonForm>
+                    </Grid>
+                    <Grid item>
+                      <ButtonForm>Add Vaccine</ButtonForm>
+                    </Grid>
+                    <Grid item>
+                      <ButtonForm>Add Vaccinator Name</ButtonForm>
+                    </Grid>
+                  </Grid>
+                </Typography>
+              </CardContent>
+            </CardContent>
+          </Card>
         </Grid>
-        <Grid item xs>
-          <ButtonForm>Add Vaccine</ButtonForm>
-        </Grid>
-        <Grid item xs>
-          <ButtonForm>Add Doctor (?)</ButtonForm>
-        </Grid>
-        <Grid item xs>
-          <Card>Announcement here</Card>
+        <Grid item>
+          <Card elevation={5} style={{ marginLeft: "1rem", padding: "1rem" }}>
+            <CardHeader title="Announcement of vaccines" />
+          </Card>
         </Grid>
       </Grid>
       {/* Graph Below */}
