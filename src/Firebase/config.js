79b56d06a9c7a11a96a8369e7149977d1e6199b4
Ceed Jennelle Lorenzo <bg201801625@wmsu.edu.ph>
2021-08-16 13:36:26 +0800

    added firebase into the project

---
+++ src/Firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDRo8PVK7iv63N8ZnodXMWF9CsVrug4hGQ",
+  authDomain: "vaccination-tracking.firebaseapp.com",
+  projectId: "vaccination-tracking",
+  storageBucket: "vaccination-tracking.appspot.com",
+  messagingSenderId: "571529961699",
+  appId: "1:571529961699:web:3f74debe3358b94edbe5d6",
+};
