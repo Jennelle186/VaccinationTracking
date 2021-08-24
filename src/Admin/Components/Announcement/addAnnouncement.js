0bd12456aa70de926a98775109fb65713526e9d6
Ceed Jennelle Lorenzo <bg201801625@wmsu.edu.ph>
2021-08-24 11:00:37 +0800

    edited the add announcement page

--- src/Admin/Components/Announcement/addAnnouncement.js
+++ src/Admin/Components/Announcement/addAnnouncement.js
@@ -1,5 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Card, makeStyles, Grid } from "@material-ui/core";
+import ButtonForm from "../../../components/Forms/Button/button";
+
+const useStyles = makeStyles({
+  root: {
+    // margin: "0 auto",
+    marginBottom: "1rem",
+    padding: "1rem",
+  },
+  link: {
+    textDecoration: "none",
+  },
+});
+
 const AddAnnouncement = () => {
-  return <div>Add Announcement</div>;
+  const classes = useStyles();
+  return (
+    <>
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Grid item>
+          <Link to="/announcement" className={classes.link}>
+            <ButtonForm>Back</ButtonForm>
+          </Link>
+        </Grid>
+      </Grid>
+      <Card className={classes.root}>Hello</Card>
+    </>
+  );
 };
 
 export default AddAnnouncement;
