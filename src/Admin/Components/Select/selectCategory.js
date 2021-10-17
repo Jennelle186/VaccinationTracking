3d1ed4a66c5392d2c50b7df9f231dacd70c138ce
Ceed Jennelle Lorenzo <bg201801625@wmsu.edu.ph>
2021-10-17 12:14:29 +0800

    edited the category

--- src/Admin/Components/Select/selectCategory.js
+++ src/Admin/Components/Select/selectCategory.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { FormControl, InputLabel, Select, MenuItem, ListSubheader } from "@material-ui/core";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  ListSubheader,
+} from "@material-ui/core";
 
 const Category = ({ value, onChange }) => {
   return (
     <div>
       <FormControl fullWidth>
         <InputLabel htmlFor="vaccinator-name">Category</InputLabel>
-        <Select value={value} onChange={onChange} fullWidth defaultValue="" id="grouped-select" label="Grouping">
-            <MenuItem value="A1">A1</MenuItem>
-            <MenuItem value="A2">A2</MenuItem>
-            <MenuItem value="A3">A3</MenuItem>
-            <MenuItem value="A4">A4</MenuItem>
-            <MenuItem value="A5">A5</MenuItem>
-            <MenuItem value="ROAP">ROAP</MenuItem>
+        <Select
+          value={value}
+          onChange={onChange}
+          fullWidth
+          defaultValue=""
+          id="grouped-select"
+          label="Grouping"
+        >
+          <MenuItem value="1">1</MenuItem>
+          <MenuItem value="2">2</MenuItem>
+          <MenuItem value="3">3</MenuItem>
+          <MenuItem value="4">4</MenuItem>
+          <MenuItem value="5">5</MenuItem>
+          <MenuItem value="ROAP">ROAP</MenuItem>
         </Select>
       </FormControl>
     </div>
