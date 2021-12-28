f5d782e5fca3a65dc1286785e4fc4433f3dface2
Ceed Jennelle Lorenzo <bg201801625@wmsu.edu.ph>
2021-12-28 10:04:07 +0800

    fixed the UI and some are in comments

--- src/pages/Homepage/homepage.js
+++ src/pages/Homepage/homepage.js
@@ -2,7 +2,9 @@ import Banner from "../../components/Layout/header2";
 import ViewAnnouncement from "../../components/View Announcement/viewAnnouncement";
 import AvailableVaccine from "../../components/AvailabilityVaccines/availableVaccine";
 import PieGraph from "../../Admin/Components/Graph/pieGraph";
-import { Card } from "@material-ui/core";
+import chartWithSelect from "../../Admin/Components/Graph/chartWithSelect";
+import { Card, Grid } from "@material-ui/core";
+import ChartWithSelect from "../../Admin/Components/Graph/chartWithSelect";
 
 const Homepage = (props) => {
   return (
@@ -10,12 +12,37 @@ const Homepage = (props) => {
       <Banner />
       <section>
         <br />
+
+        {/* <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <Card style={{ padding: "1rem", margin: "1rem" }} elevation={10}>
+              <ViewAnnouncement />
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Card style={{ padding: "1rem", margin: "1rem" }} elevation={10}>
+              <AvailableVaccine />
+            </Card>
+          </Grid>
+        </Grid> */}
+
         <ViewAnnouncement />
         <AvailableVaccine />
-        <Card style={{ padding: "1rem", margin: "1rem" }} elevation={10}>
-          {" "}
-          <PieGraph />
-        </Card>
+
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={6}>
+            <Card style={{ padding: "1rem", margin: "1rem" }} elevation={10}>
+              <PieGraph />
+            </Card>
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Card style={{ padding: "1rem", margin: "1rem" }} elevation={10}>
+              <ChartWithSelect />
+            </Card>
+          </Grid>
+        </Grid>
       </section>
     </div>
   );
